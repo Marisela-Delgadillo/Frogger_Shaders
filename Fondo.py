8805f904552468d82fadfc86756543856672dabb
unknown <marisela.delgadillo.cbtis37@gmail.com>
2022-05-26 02:41:48 -0700

    Publish

--- Fondo.py
+++ Fondo.py
@@ -78,8 +78,8 @@ class Fondo(Modelo):
                 -0.9,0.05,0.0,1.0,         1.0, 1.0, 1.0, 1.0, #der. arriba
                 -0.9,-0.05,0.0,1.0,       1.0, 1.0, 1.0, 1.0,  #der. abajo
 
-                -0.8,0.05,0.0, 1.0,       1.0, 1.0, 1.0, 1.0, #izq. arriba
-                -0.8,-0.05,0.0,1.0,       1.0, 1.0, 1.0, 1.0, #izq. abajo
+                -0.3,0.05,0.0, 1.0,       1.0, 1.0, 1.0, 1.0, #izq. arriba
+                -0.3,-0.05,0.0,1.0,       1.0, 1.0, 1.0, 1.0, #izq. abajo
                 -0.2,0.05,0.0,1.0,         1.0, 1.0, 1.0, 1.0, #der. arriba
                 -0.2,-0.05,0.0,1.0,       1.0, 1.0, 1.0, 1.0,  #der. abajo
 
@@ -176,18 +176,78 @@ class Fondo(Modelo):
                 1.1, 1.0,0.0,1.0,           0, 0, 0,1.0, #derecha arriba
                 1.1, 0.9,0.0,1.0,           0, 0, 0,1.0, # derecha abajo 
 
+                #peatonal
+                -1.5,-0.25,0.0, 1.0,       1, 1, 1, 1, #izq. arriba
+                -1.5,-0.28,0.0,1.0,       1, 1, 1, 1, #izq. abajo
+                -0.9,-0.25,0.0,1.0,         1, 1, 1, 1, #der. arriba
+                -0.9,-0.28,0.0,1.0,       1, 1, 1, 1,  #der. abajo
+
+                -1.5,-0.20,0.0, 1.0,       1, 1, 1, 1, #izq. arriba
+                -1.5,-0.23,0.0,1.0,       1, 1, 1, 1, #izq. abajo
+                -0.9,-0.20,0.0,1.0,         1, 1, 1, 1, #der. arriba
+                -0.9,-0.23,0.0,1.0,       1, 1, 1, 1,  #der. abajo
+
+                -1.5,-0.15,0.0, 1.0,       1, 1, 1, 1, #izq. arriba
+                -1.5,-0.18,0.0,1.0,       1, 1, 1, 1, #izq. abajo
+                -0.9,-0.15,0.0,1.0,         1, 1, 1, 1, #der. arriba
+                -0.9,-0.18,0.0,1.0,       1, 1, 1, 1,  #der. abajo
+
+                -1.5,-0.10,0.0, 1.0,       1, 1, 1, 1, #izq. arriba
+                -1.5,-0.13,0.0,1.0,       1, 1, 1, 1, #izq. abajo
+                -0.9,-0.10,0.0,1.0,         1, 1, 1, 1, #der. arriba
+                -0.9,-0.13,0.0,1.0,       1, 1, 1, 1,  #der. abajo
+
+                -1.5,-0.05,0.0, 1.0,       1, 1, 1, 1, #izq. arriba
+                -1.5,-0.08,0.0,1.0,       1, 1, 1, 1, #izq. abajo
+                -0.9,-0.05,0.0,1.0,         1, 1, 1, 1, #der. arriba
+                -0.9,-0.08,0.0,1.0,       1, 1, 1, 1,  #der. abajo
+
+                ###################################################
+
+                -1.5,0.25,0.0, 1.0,       1, 1, 1, 1, #izq. arriba
+                -1.5,0.28,0.0,1.0,       1, 1, 1, 1, #izq. abajo
+                -0.9,0.25,0.0,1.0,         1, 1, 1, 1, #der. arriba
+                -0.9,0.28,0.0,1.0,       1, 1, 1, 1,  #der. abajo
+
+                -1.5,0.20,0.0, 1.0,       1, 1, 1, 1, #izq. arriba
+                -1.5,0.23,0.0,1.0,       1, 1, 1, 1, #izq. abajo
+                -0.9,0.20,0.0,1.0,         1, 1, 1, 1, #der. arriba
+                -0.9,0.23,0.0,1.0,       1, 1, 1, 1,  #der. abajo
+
+                -1.5,0.15,0.0, 1.0,       1, 1, 1, 1, #izq. arriba
+                -1.5,0.18,0.0,1.0,       1, 1, 1, 1, #izq. abajo
+                -0.9,0.15,0.0,1.0,         1, 1, 1, 1, #der. arriba
+                -0.9,0.18,0.0,1.0,       1, 1, 1, 1,  #der. abajo
+
+                -1.5,0.10,0.0, 1.0,       1, 1, 1, 1, #izq. arriba
+                -1.5,0.13,0.0,1.0,       1, 1, 1, 1, #izq. abajo
+                -0.9,0.10,0.0,1.0,         1, 1, 1, 1, #der. arriba
+                -0.9,0.13,0.0,1.0,       1, 1, 1, 1,  #der. abajo
+
+                -1.5,0.05,0.0, 1.0,       1, 1, 1, 1, #izq. arriba
+                -1.5,0.08,0.0,1.0,       1, 1, 1, 1, #izq. abajo
+                -0.9,0.05,0.0,1.0,         1, 1, 1, 1, #der. arriba
+                -0.9,0.08,0.0,1.0,       1, 1, 1, 1,  #der. abajo
+
                 #alcantarilla
-                0.0,-0.2,0.0,1.0,  0,0.1,0.1,0,
+                #0.0,-0.2,0.0,1.0,  0,0.1,0.1,0
             ], dtype="float32"
 
             
         ))
-
+        #Alcantarilla
         for i in range(0,361,5):
             self.vertices = np.append(self.vertices,
                 np.array(
                     [0.08 * math.cos(i * math.pi/180.0),0.08 * math.sin(i * math.pi/180) -0.2,0.0,1.0,  0,0.1,0.1,0]
                 , dtype="float32"))
+        #Nenufar
+        for angulo in range(0, 359, 5):
+            componente_x = 0.04 * math.cos(angulo * math.pi / 180) + 0.4
+            componente_y =  0.025 * math.sin(angulo * math.pi / 180) - 0.95
+
+            self.vertices = np.append(self.vertices, np.array([componente_x, componente_y, 0.0 , 1.0, 
+                                                       40/255, 121/255, 17/255, 1.0 ], dtype="float32"))
 
         self.posicion = glm.vec3(0,0,0)
         self.transformaciones = glm.mat4(1.0)
@@ -214,18 +274,6 @@ class Fondo(Modelo):
         gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 12, 4)
         gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 16, 4)
         gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 20, 4)
-        # gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 24, 4)
-        # gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 28, 4)
-        # gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 32, 4)
-        # gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 36, 4)
-        # gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 40, 4)
-        # gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 44, 4)
-        # gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 48, 4)
-        # gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 52, 4)
-        # gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 56, 4)
-        # gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 60, 4)
-        # gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 64, 4)
-        # gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 68, 4)
         gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 72, 4)
         gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 76, 4)
         gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 80, 4)
@@ -238,7 +286,18 @@ class Fondo(Modelo):
         gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 108, 4)
         gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 112, 4)
         gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 116, 4)
-        gl.glDrawArrays(gl.GL_TRIANGLE_FAN, 120, 74)
+        gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 120, 4)
+        gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 124, 4)
+        gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 128, 4)
+        gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 132, 4)
+        gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 136, 4)
+        gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 140, 4)
+        gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 144, 4)
+        gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 148, 4)
+        gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 152, 4)
+        gl.glDrawArrays(gl.GL_TRIANGLE_STRIP, 156, 4)
+        gl.glDrawArrays(gl.GL_TRIANGLE_FAN, 160, 74)
+        gl.glDrawArrays(gl.GL_TRIANGLE_FAN, 234, 70)
 
 
 
